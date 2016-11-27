1ffeddffdbb633f3b6ee518704626d0bb041a1e1
Vu Nguyen <Vu Nguyen>
2016-11-27 21:12:34 +0700

    update

--- assets/materialize/setting.js
+++ assets/materialize/setting.js
@@ -14,7 +14,7 @@ var colors = [
     // "brown", "grey", "blue-grey"      // 16 17 18 19
 ];
 
-colors = ['green', 'deep-purple']
+// colors = ['green', 'deep-purple']
 var i = Math.floor( Math.random() * (colors.length - 0) + 0 );
 var chooseColor = colors[i].toString();
 
