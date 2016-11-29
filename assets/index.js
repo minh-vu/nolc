78594180ddcc138da8b29d5b383f39e8ea316e20
Vu Nguyen <Vu Nguyen>
2016-11-29 16:33:20 +0700

    update

--- assets/index.js
+++ assets/index.js
@@ -88,6 +88,14 @@ function styleControls(){
 
     // $("#your-fb.fb-comments").attr('data-href', URL); //set truoc
     $("span#current-color").html(chooseColor.toUpperCase());
+
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
 }
 
 function checkLicense(url){
